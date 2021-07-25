39b22ae60f8f3cdf4268183ce039539b0c07d18e
jose-izam99 <57118197+jose-izam99@users.noreply.github.com>
2021-07-25 05:24:23 -0400

    add postgres credentials

--- joseizam/settings.py
+++ joseizam/settings.py
@@ -77,12 +77,7 @@ WSGI_APPLICATION = 'joseizam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = eval(os.environ.get('DATABASES_APP_JOSEIZAM'))
 
 
 # Password validation

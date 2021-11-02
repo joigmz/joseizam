c05e3316a0721d03966d3a6393c1ce2eee696acd
jose-izam99 <57118197+jose-izam99@users.noreply.github.com>
2021-11-02 15:26:25 -0300

    resume sin cosas fancys

--- resume/views.py
+++ resume/views.py
@@ -7,6 +7,3 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "index.html")
-
-def index2(request):
-    return render(request, "index2.html")

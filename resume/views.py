2ca351221989b2cff774b6322f373560fd6f7e24
jose-izam99 <57118197+jose-izam99@users.noreply.github.com>
2021-12-02 01:58:27 -0300

    add project page

--- resume/views.py
+++ resume/views.py
@@ -7,3 +7,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def project(request):
+    return render(request, "proyecto.html")

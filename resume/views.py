c43b0eb8cbc8e71723ed98880af21fa6a90cd117
jose-izam99 <57118197+jose-izam99@users.noreply.github.com>
2021-12-27 12:45:39 -0300

    a

--- resume/views.py
+++ resume/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 import os
-import tweepy
 
 
 # Create your views here.
@@ -12,15 +11,5 @@ def index(request):
     return render(request, "index.html")
 
 def project(request):
-    bearer_token = os.environ.get('bearer_token_twitter_JOSEIZAM')
-    client = tweepy.Client(bearer_token=bearer_token, return_type=dict)
 
-
-    tweets = client.search_recent_tweets(query='IzkiaxBoricMV')
-
-    context = {
-       'data': tweets['data'],
-       'hashtag': 'IzkiaxBoricMV'
-    }
-
-    return render(request, "proyecto.html", context=context)
+    return render(request, "proyecto.html")
